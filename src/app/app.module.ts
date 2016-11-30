24692d72146cfe5efe146506fd98a43d6a4de455
richymarin <ricardomh24@hotmail.com>
2016-11-29 21:14:13 -0500

    subir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { EstudianteComponent } from './estudiante/estudiante.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EstudianteComponent
+    EstudianteComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
